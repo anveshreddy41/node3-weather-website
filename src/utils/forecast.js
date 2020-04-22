63366d5fec46754094752723c6dc70fab9f2acaa
Anvesh kumar Reddy Bheemi Reddy <anveshkumarreddybheemireddy@anveshs-mbp.home>
2020-04-22 23:00:18 +0100

    Changing forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude,callback) => {
         } else if (body.error) {
             callback('Unable to find location.')
         } else {
-            callback(undefined,'It is currently '+body.current.temperature+' degrees out. There is a '+ body.current.precip +'% chance of rain.')
+            callback(undefined,'It is currently '+body.current.temperature+' degrees out. There is a '+ body.current.precip +'% chance of rain and the humidity is '+body.current.humidity+'%.')
         }
     })
 
